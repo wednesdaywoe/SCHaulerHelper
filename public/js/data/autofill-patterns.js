4c2519285905cb09a148bb28e0ae4bffcbf35e53
Wednesdaywoe <wednesday.woe@gmail.com>
2026-01-30 08:44:22 -0500

    Remove legacy vanilla codebase

--- public/js/data/autofill-patterns.js
+++
@@ -1,8 +0,0 @@
-/**
- * AUTOFILL PATTERNS - Star Citizen Hauler Helper
- */
-const AUTOFILL_PATTERNS = {};
-
-if (typeof window !== 'undefined') {
-    window.AUTOFILL_PATTERNS = AUTOFILL_PATTERNS;
-}
